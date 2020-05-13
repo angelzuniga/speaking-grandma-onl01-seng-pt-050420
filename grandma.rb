15800611bfdc981616230ba9b234fa8f696ae8dd
Angel Zuniga <angelzuniga@outlook.com>
2020-05-13 08:19:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,11 +12,14 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma(phrase)
-
+ef speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  
   if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  else 
+     return "HUH?! SPEAK UP, SONNY!"
+  else
     return "NO, NOT SINCE 1938!"
   end
 end

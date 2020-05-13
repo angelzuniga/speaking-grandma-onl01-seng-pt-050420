deb20321a43ddc3667823c3eac134ecd1f363f9d
Angel Zuniga <angelzuniga@outlook.com>
2020-05-13 08:14:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 speak_to_grandma(phrase)
 
-if phrase !== phrase.upcase
-  return "HUH?! SPEAK UP, SONNY!"
-else 
+  if phrase !== phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    puts "NO, NOT SINCE 1938!"
+  end
+end
